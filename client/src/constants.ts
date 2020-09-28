51d51305912b32238cc12d63138f0a30db633079
Aakash <9267aakashsharma@gmail.com>
2020-09-28 06:02:10 +0530

    feat(home): add logo and url
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,14 +1,35 @@
 import ZUBI_LOGO_WHITE from "./assets/images/logos/zubi_white.png";
 import ZUBI_LOGO_BLACK from "./assets/images/logos/zubi_black.png";
-import HERO_IMG from "./assets/images/home/hero.png";
+import ZUBI_LOGO_WHITE_SVG from "./assets/images/logos/zubi_white.svg";
+import HERO_IMG from "./assets/images/home/hero.svg";
 
 export const Assets = {
   ZUBI_LOGO_WHITE,
   ZUBI_LOGO_BLACK,
+  ZUBI_LOGO_WHITE_SVG,
   HERO_IMG,
 };
 
 export const RedirectURL = {
-  Blog: "https://blog.zubi.io/",
-  Hiring: "https://forum.zubi.io/c/discover/job-board",
+  DownloadApp: "https://play.google.com/store/apps/details?id=com.zubiapp",
+  Zubi: {
+    About: "https://zubi.io/aboutus",
+    Asses: "https://assess.zubi.io/",
+    Blog: "https://blog.zubi.io/",
+    Carrers: "https://angel.co/company/zubi-1",
+    Certificates: "https://certz.zubi.io/",
+    Contact: "mailto:contact@zubi.io",
+    Discover: "https://discover.zubi.io/",
+    Events: "https://events.zubi.io/",
+    Hiring: "https://forum.zubi.io/c/discover/job-board",
+    Learn: "https://learn.zubi.io/",
+  },
+  Socials: {
+    Facebook: "https://www.facebook.com/zubi.io/",
+    Telegram: "https://t.me/zubi_io",
+    Instagram: "https://www.instagram.com/zubi.io/",
+    Linkedin: "https://www.linkedin.com/showcase/zubi-org/",
+    Twitter: "https://twitter.com/zubi_io",
+    Youtube: "https://www.youtube.com/channel/UC5Cl0ZfRIfnfQSwen3w1d6Q",
+  },
 };
