4d47489e7ee6f4626ac450b0162236a0dab3e09b
Aakash <9267aakashsharma@gmail.com>
2020-09-28 06:05:21 +0530

    feat(home): add hero footer
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

--- client/src/shared/components/Footer/Footer.tsx
+++ client/src/shared/components/Footer/Footer.tsx
@@ -1,7 +1,106 @@
 import React from "react";
+import {
+  FaFacebook,
+  FaTelegramPlane,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import { Assets, RedirectURL } from "../../../constants";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div className="zubi-footer">footer</div>;
+  return (
+    <div className="zubi-footer">
+      <div className="inner">
+        <div className="logo">
+          <img src={Assets.ZUBI_LOGO_WHITE_SVG} alt="ZUBI_LOGO" />
+          <h2>Zubi.io</h2>
+        </div>
+        <div className="content">
+          <div>
+            <h2>Company</h2>
+            <a href={RedirectURL.Zubi.About}>About</a>
+            <a href={RedirectURL.Zubi.Carrers}>Carrers</a>
+            <a href={RedirectURL.Zubi.Hiring}>Hiring</a>
+          </div>
+          <div>
+            <h2>Products</h2>
+            <a href={RedirectURL.Zubi.Asses}>Z.Assess</a>
+            <a href={RedirectURL.Zubi.Learn}>Z.Learn</a>
+            <a href={RedirectURL.Zubi.Certificates}>Z.Certz</a>
+          </div>
+          <div>
+            <h2>Contact Us</h2>
+            <p>Corp. Office:</p>
+            <p>A3, Sector 04, Noida,</p>
+            <p> Uttar Pradesh - 201301.</p>
+            <a href={RedirectURL.Zubi.Contact}>E-mail: contact@zubi.io</a>
+          </div>
+        </div>
+      </div>
+      <div className="zubi-copyright">
+        <small>&copy; Zubi Infotech Pvt. Ltd. 2020</small>
+        <ul>
+          <li>
+            <a
+              href={RedirectURL.Socials.Facebook}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a
+              href={RedirectURL.Socials.Telegram}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTelegramPlane />
+            </a>
+          </li>
+          <li>
+            <a
+              href={RedirectURL.Socials.Instagram}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href={RedirectURL.Socials.Linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a
+              href={RedirectURL.Socials.Twitter}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a
+              href={RedirectURL.Socials.Youtube}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
