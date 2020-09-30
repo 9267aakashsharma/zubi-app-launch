07099a612ca1d3d07d754dd2114a64a72da8b66a
Aakash <9267aakashsharma@gmail.com>
2020-09-30 10:26:57 +0530

    feat(home): add event section
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

---
+++ client/src/modules/Home/Events/Events.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Assets } from "../../../constants";
+import "./Events.scss";
+
+const Events = () => {
+  return (
+    <div className="zubi-events">
+      <div className="zubi-static-bubbles">
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+      </div>
+      <div className="content">
+        <p>
+          Learn from Pro and build a project with a professional mentor. Prepare
+          for interviews, polish your resume, and get hired by emerging
+          technology companies faster. Our mentors help you get more interviews
+          and ace them for top tech companies and emerging technology roles.
+        </p>
+        <a href="/">Explore Events</a>
+      </div>
+      <div className="image">
+        <img src={Assets.PHONE_EVENT} alt="PHONE_EVENT" />
+      </div>
+    </div>
+  );
+};
+
+export default Events;
