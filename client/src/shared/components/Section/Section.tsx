774334309da625e8cdfafb6f97265f294ecc6774
Aakash <9267aakashsharma@gmail.com>
2020-09-30 10:23:39 +0530

    feat(home): add theme to section
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

--- client/src/shared/components/Section/Section.tsx
+++ client/src/shared/components/Section/Section.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import classNames from "classnames";
+import { styleTheme } from "../../../App";
 import "./Section.scss";
 
 interface Props {
   child: React.ReactNode;
+  bgColor?: string;
 }
 
 const Section = (props: Props) => {
-  return <div className="zubi-section">{props.child}</div>;
+  return (
+    <div
+      className={classNames("zubi-section", {
+        bgSecondary: props.bgColor === "secondary",
+      })}
+      style={styleTheme}
+    >
+      {props.child}
+    </div>
+  );
 };
 
 export default Section;
