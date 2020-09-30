e837c48b145f988c4397f4931c086b70907b2562
Aakash <9267aakashsharma@gmail.com>
2020-09-30 10:25:48 +0530

    feat(home): add industry section
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

---
+++ client/src/modules/Home/Industry/Industry.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Assets, RedirectURL } from "../../../constants";
+import "./Industry.scss";
+
+const Industry = () => {
+  return (
+    <div className="zubi-industry">
+      <div className="content">
+        <h1>Straight From Industry Leaders to You. Nailed it</h1>
+        <p>
+          Our learning modules are skills-based, not time-based. Expect the best
+          live-streamed content, asynchronous curriculum, industry-vetted group
+          projects, and one-on-one coaching and career support – all tailored
+          for your success.
+        </p>
+        <a href={RedirectURL.DownloadApp}>Download the APP</a>
+      </div>
+      <div className="image">
+        <img src={Assets.TESTING} alt="TESTING" />
+      </div>
+    </div>
+  );
+};
+
+export default Industry;
